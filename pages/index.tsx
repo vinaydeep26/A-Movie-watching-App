345dbb9bc64bbe072ef6fd2ecdf244410dcc4821
vinaydeep26 <vinaydeep26@gmail.com>
2023-05-03 17:58:23 -0400

    modified:   .env 	new file:   hooks/useCurrentUser.ts 	new file:   lib/fetcher.ts 	new file:   lib/serverAuth.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/api/auth/[...nextauth].ts 	new file:   pages/api/current.ts 	modified:   pages/auth.tsx 	modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
+import { getSession,signOut } from "next-auth/react"
+import { NextPageContext } from "next";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+
+  const {data:user } = useCurrentUser();
   return (
     <>
-    <h1 className="text-2xl text-green-500">netphlix</h1></>
+    <h1 className="text-5xl text-green-500">netphlix</h1>
+    <p className="text-white"> Logged in as : {user?.email}</p>
+    <button className="h-10 w-full bg-white" onClick={() => signOut()}> logout! </button>
+    </>
   )
 }
