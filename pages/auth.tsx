9e4e4aad6369ecf51a60732aff94829e775725f1
vinaydeep26 <vinaydeep26@gmail.com>
2023-05-02 22:11:15 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   pages/auth.tsx
    	new file:   public/images/frens.jpg
    	new file:   public/images/frontPage.jpg
    	new file:   public/images/logo2.jpg
    	new file:   public/images/logo3.png
    	new file:   public/images/logo4.png
    
    Changes not staged for commit:
    	deleted:    public/images/frens.jpg
    	deleted:    public/images/hero.jpg
    	deleted:    public/images/logo.png
    	deleted:    public/images/logo2.jpg
    	deleted:    public/images/logo3.png

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,30 +1,36 @@
 import Input from '@/components/Input'
-import React, { use, useState } from 'react'
+import React, {  useCallback, useState } from 'react'
 
 const Auth = () => { 
     const [email,setEmail] = useState('');
     const [name,setName] = useState('');
     const [password,setPassword] = useState('');
-    const [variant,setVariant] = useState('');
+    const [variant,setVariant] = useState('login');
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant == 'login' ? 'register' : 'login');
+    },[])
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="relative h-full w-full bg-[url('/images/frontPage.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
         <div className="bg-black w-full h-full lg:bg-opacity-50">
             <nav className="px-12 py-5">
-                <img src="/images/logo.png" alt="" className="h-12" />
+                <img src="/images/logo4.png" alt="" className="h-12" />
             </nav>
             <div className="flex justify-center">
                 <div className="bg-black bg-opacity-70px px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                     <h2 className="text-white text-4xl mb-8 font-semibold">
-                        Sign in
+                        {variant === 'login' ? 'Sign in' : 'Register'}
                     </h2>
                     <div className="flex flex-col gap-4">
-                    <Input 
-                        label="username"
-                        id="name"
-                        onChange={(ev:any) => setName(ev.target.value)}
-                        value={name}
-                        />
+                    {variant === 'register' && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)} 
+                />
+              )}
                     <Input 
                         label="email"
                         type="email"
@@ -40,12 +46,14 @@ const Auth = () => {
                         value={password}
                         />
                     </div>
-                    <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                        Login
+                    <button className="bg-yellow-600 py-3 text-white rounded-md w-full mt-10 hover:bg-blue-700 transition">
+                    {variant === 'login' ? 'Login' : 'Sign up'}
                     </button>
                     <p className="text-neutral-500 mt-12">
-                        First time boi? 
-                        <span className="text-white ml-1 hover:underline cursor-pointer"> Create Account</span>
+                    {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'} 
+                        <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                        {variant === 'login' ? 'Create an account' : 'Login'}
+                        </span>
                     </p>
                 </div>  
             </div>
