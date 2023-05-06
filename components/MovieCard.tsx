10cd88eaf04aa682ac0e29b9002b3586facb2683
vinaydeep26 <vinaydeep26@gmail.com>
2023-05-05 21:45:34 -0400

    modified:   .eslintrc.json 	modified:   components/MovieCard.tsx

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -18,7 +18,7 @@ interface MovieCardProps {
         shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]" />
       <div className="
         opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110
-        group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
+        group-hover:-translate-y-[6vw] group-hover:translate-x-[1vw] group-hover:opacity-100">
         <img src={data.thumbnailUrl} alt="Movie" draggable={false} className="
           cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]" />
         <div className=" z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
